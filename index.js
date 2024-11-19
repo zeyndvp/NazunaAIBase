8231cc37d5fbf7d9547a72a762cd14a763a91aad
zeyndvp <lanagans53@gmail.com>
2024-11-19 22:26:32 +0700

    del env

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
-const PORT = process.env.PORT || 8080
+const PORT = 8080
 const client = require('./client')
 
 const runServer = async () => {
