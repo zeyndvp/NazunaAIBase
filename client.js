d2031ed3a1f4211e846da3308f805d6c110eb372
zeyndvp <lanagans53@gmail.com>
2024-11-19 22:24:26 +0700

    msg to m

--- client.js
+++ client.js
@@ -56,33 +56,33 @@ async function connectToWhatsapp() {
 
     // Event Messages
     sock.ev.on("messages.upsert", async ({ messages }) => {
-        const msg = messages[0];
-        if (!msg.message) return;
+        global.m = messages[0];
+        if (!m.message) return;
         // Message Jid
-        msg.jid = msg.key.remoteJid;
+        m.jid = m.key.remoteJid;
         // Jika Sebuah Status Kita Tambahkan Auto Read Status
-        if (msg.jid === "status@broadcast") return sock.readMessages([msg.key]);
+        if (m.jid === "status@broadcast") return sock.readMessages([m.key]);
         // Message From Group
-        msg.isGroup = msg.jid.endsWith("@g.us");
+        m.isGroup = m.jid.endsWith("@g.us");
         // User Jid || Jika Dari Group Kita Ambil Dari Participant Namun Jika Bukan Dari Group, Kita Ambil Dari remoteJid
-        msg.userJid = msg.isGroup ? msg.key.participant : msg.key.remoteJid // Atau msg.jid
+        m.userJid = m.isGroup ? m.key.participant : m.key.remoteJid // Atau m.jid
         // Nmae Dari Profile Pengirim
-        msg.userName = msg.pushName
+        m.userName = m.pushName
         // Jika Pesan Dari Bot
-        msg.fromMe = msg.key.fromMe
+        m.fromMe = m.key.fromMe
         // Type Dari Sebuah Pesan
-        msg.type = Object.keys(msg.message)[0]
+        m.type = Object.keys(m.message)[0]
         // Pesan Text & Jika Pesan extendedTextMessage Kita Ambil Textnya & Jika Pesan Conversation Kita Ambil Conversation
-        msg.text = msg.type === "extendedTextMessage" 
-        ? msg.message.extendedTextMessage.text 
-        : msg.type === "conversation" 
-        ? msg.message.conversation 
-        : msg.type === "imageMessage" 
-        ? msg.message.imageMessage.caption 
+        m.text = m.type === "extendedTextMessage" 
+        ? m.message.extendedTextMessage.text 
+        : m.type === "conversation" 
+        ? m.message.conversation 
+        : m.type === "imageMessage" 
+        ? m.message.imageMessage.caption 
         : "";
-        if (!msg.fromMe || msg.text !== 'p') return
-        const sendMessage = await sock.sendMessage(msg.jid, { text: 'https://github.com/zeyndvp'}, { quoted: msg })
-        console.log(msg)
+        if (!m.fromMe || m.text !== 'p') return
+        const sendMessage = await sock.sendMessage(m.jid, { text: 'https://github.com/zeyndvp'}, { quoted: m })
+        console.log(m)
     })
 
     sock.ev.on("group-participants.update", (group) => {
